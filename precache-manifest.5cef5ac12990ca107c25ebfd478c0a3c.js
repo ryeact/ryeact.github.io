f88030eaaef210574b10528a232a931ad97de2b4
Ary Harlestary <aryharlestary@gmail.com>
2020-08-05 22:49:32 +0700

    Menyalin isi folder build

---
+++ precache-manifest.5cef5ac12990ca107c25ebfd478c0a3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5c87a681f9cdd544e83",
+    "url": "/static/css/main.4039634e.chunk.css"
+  },
+  {
+    "revision": "c5c87a681f9cdd544e83",
+    "url": "/static/js/main.c5c87a68.chunk.js"
+  },
+  {
+    "revision": "db21676f565284536e1f",
+    "url": "/static/js/1.db21676f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e6cbebd5dae77fc84c537691ab2a58f",
+    "url": "/index.html"
+  }
+];
